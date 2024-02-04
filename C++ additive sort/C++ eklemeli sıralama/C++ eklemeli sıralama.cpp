63bcce2841b900bd72f6a31aa6327f5ddacfb002
berke erdem <berkeerdem0@gmail.com>
2024-02-04 23:19:35 +0300

    additive sort

---
+++ C++ additive sort/C++ eklemeli sıralama/C++ eklemeli sıralama.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+void İnsertionSort(int* sayilar, int uzunluk)
+{
+    int i;
+    for (int j = 1;j < uzunluk;j++) {
+        int secilen = sayilar[j];
+        for (i = j - 1;(i >= 0) && (sayilar[i] > secilen);i--) 
+            {
+                sayilar[i + 1] = sayilar[i];
+
+            }
+            sayilar[i + 1] = secilen;
+        }
+
+    }
